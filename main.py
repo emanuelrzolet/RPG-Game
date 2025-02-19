6cf8f01c08afa51f3532246ff94e85148b326501
Emanuel Rosa Zolet <140463663+emanuelrzolet@users.noreply.github.com>
2025-02-19 08:53:58 -0300

    Create main
    Criado arquivo main e a configuração do botão de fechar a janela

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+# Inicialização do pygame e da janela
+print("Sistem start")
+pygame.init()
+screen = pygame.display.set_mode(size=(600,480))
+print("Sistem end")
+print("Loop start")
+while True:
+    # Check for all Events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() #Close screen
+            quit() #end pygame
