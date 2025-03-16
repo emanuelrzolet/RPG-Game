ae1190a804d3d6a7a80654297f213e7ec3f70f9b
Emanuel Rosa Zolet <140463663+emanuelrzolet@users.noreply.github.com>
2025-03-16 10:41:40 -0300

    Feito implementação da musica

--- code/game.py
+++ code/game.py
@@ -5,19 +5,28 @@ from code.menu import Menu, Level
 class Game:
     def __init__(self):
         pygame.init()
+        pygame.mixer.init() # Inicializa o mixer de áudio
         info = pygame.display.Info()
         largura = info.current_w / 2
         altura = info.current_h / 2
         self.window = pygame.display.set_mode((largura, altura))
-        self.game_state = "menu"  # Estado inicial do jogo
+        self.game_state = "menu"
+        self.musica_tocando = False # Adiciona a variável de controle da música
+
+    def tocar_musica(self):
+        pygame.mixer.music.load("./assets/music.mp3") # Carrega a música
+        pygame.mixer.music.play(-1) # Toca a música em loop
 
     def run(self):
         while True:
             if self.game_state == "menu":
+                if not self.musica_tocando: # Verifica se a música já está tocando
+                    self.tocar_musica() # Toca a música
+                    self.musica_tocando = True
                 menu = Menu(self.window)
                 if menu.run():
-                    self.game_state = "level"  # Muda para o estado do nível
+                    self.game_state = "level"
             elif self.game_state == "level":
                 level = Level(self.window, name="Level1")
-                level.game_loop() # inicia o loop do level
-                self.game_state = "menu" # retorna ao menu após o loop do level terminar
+                level.game_loop()
+                self.game_state = "menu"
